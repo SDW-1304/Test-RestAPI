0e6fe9848c0b6b3d767a026b785b60e4f26c591a
ego8973 <ego8973@gmail.com>
2024-07-18 22:28:53 +0300

    add database

---
+++ src/test/java/com/example/Test_RestAPI/TestRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test_RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
